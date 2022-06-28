2f4d5375d77bdf441004247f5faaa3f7fcd6b7d4
varun yadav <varun@keyninja.io>
2022-06-28 23:28:43 +1000

    s

--- omdena/_dataprocessing.py
+++ omdena/_dataprocessing.py
@@ -12,7 +12,15 @@ def process():
     
     # Fix Categorical variable
     df[['State_Factor','building_class','facility_type']] = df[['State_Factor','building_class','facility_type']].astype('category')
-    facility = []
+    facility = ['Commercial','Education','Food','Health','Industrial','Lodging','Multifamily','Office','Public_Assembly','Public_Safety','Retail','Service','Warehouse']
+    facility1 = ['Parking','Laboratory','Nursing','Building','Data','Religious']
+    categorytype = ['Commercial','Education','Food','Health','Industry','Lodging','Multifamily','Office','Public_area','Public_safety','Retail','Service','Warehouse']
+    categorytype1 = ['Commercial','Health','Lodging','Multifamily','Office','Public_area']
+    for i, j in zip(facility,categorytype):
+        df.loc[df['facility_type'].str.contains(i,case=False), 'facility_type_1'] = j
+    for i1, j1 in zip(facility1,categorytype1):
+        df.loc[df['facility_type'].str.contains(i1,case=False), 'facility_type_1'] = j1
+    df.fillna({'facility_type_1':'Multifamily'},inplace=True)
     return df
 
 x = process()
